611b9ac47301dcdcc2203b2f44405c5e52a9f23c
WPH2017 <2974769672@qq.com>
2017-11-01 15:18:03 +0800

    重新

--- projects/vnshop.sh
+++ projects/vnshop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
  
-WEB_PATH='/home/wwwroot/git.wphkj.cn/vnshop/'
-WEB_PATH_CLIENT='/home/wwwroot/git.wphkj.cn/vnshop/client'
+WEB_PATH='/home/wwwroot/vnshop/'
+WEB_PATH_CLIENT='/home/wwwroot/vnshop/client'
 WEB_USER='www'
 WEB_USERGROUP='www'
 # we can do 
