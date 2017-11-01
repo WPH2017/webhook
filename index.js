fe48c456b1dc91d79bbf4d237c7bb550b06f162c
WPH2017 <2974769672@qq.com>
2017-11-01 14:41:18 +0800

    好吧

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const http = require('http')
-    // const shell = require('shelljs')
-const exec = require('child_process').execFile
+const shell = require('shelljs')
 const createHandler = require('github-webhook-handler')
 const handler = createHandler({ path: '/webhook', secret: 'vnshop' })
     // 上面的 secret 保持和 GitHub 后台设置的一致
@@ -14,14 +13,11 @@ const projectHandler = (event, action) => {
     const branch = event.payload.ref
     if (projects.includes(project)) {
         console.log(new Date(), `Received a ${action} event for ${project} to ${branch}`)
-
-        exec(`./projects/${project}.sh`)
-            // shell.exec(`sh ./projects/${project}.sh`, (code, stdout, stderr) => {
-            //     console.log(new Date(), 'Exit code:', code)
-            //         // console.log(new Date(), 'Program output:', stdout)
-            //     console.log(new Date(), '执行完毕！错误信息：？', stderr)
-            // })
-
+        shell.exec(`sh ./projects/${project}.sh`, (code, stdout, stderr) => {
+            console.log(new Date(), 'Exit code:', code)
+                // console.log(new Date(), 'Program output:', stdout)
+            console.log(new Date(), '执行完毕！错误信息：？', stderr)
+        })
     }
 }
 
@@ -32,12 +28,11 @@ http.createServer((req, res) => {
     })
 }).listen(port, () => {
     console.log(new Date(), `Deploy server Run！port at ${port}`)
-        // shell.exec('echo shell test OK!', (code, stdout, stderr) => {
-        //     // console.log('Exit code:', code)
-        //     // console.log('Program output:', stdout)
-        //     // console.log('Program stderr:', stderr, stderr === '', !!stderr)
-
-    // })
+    shell.exec('echo shell test OK!', (code, stdout, stderr) => {
+        // console.log('Exit code:', code)
+        // console.log('Program output:', stdout)
+        // console.log('Program stderr:', stderr, stderr === '', !!stderr)
+    })
 })
 
 handler.on('error', err => {
