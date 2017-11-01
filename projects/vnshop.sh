adfc51bbb53ba3ace399acb14200d31b4892aa76
WPH2017 <2974769672@qq.com>
2017-11-01 15:54:38 +0800

    有啥区别？

--- projects/vnshop.sh
+++ projects/vnshop.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-
+ 
 WEB_PATH='/home/wwwroot/vnshop/'
 WEB_PATH_CLIENT='/home/wwwroot/vnshop/client'
 WEB_USER='www'
 WEB_USERGROUP='www'
-# we can do 
-echo "Start deployment vx.itnote.cn"
+ 
+echo "Start deployment shudong.wang"
 cd $WEB_PATH
 echo "pulling source code..."
 # git reset --hard origin/release
