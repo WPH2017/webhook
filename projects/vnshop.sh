963e71451afe4ac7383c314f2aa4438b61173e56
WPH2017 <2974769672@qq.com>
2017-11-01 15:33:59 +0800

    。。

--- projects/vnshop.sh
+++ projects/vnshop.sh
@@ -5,18 +5,15 @@ WEB_PATH_CLIENT='/home/wwwroot/vnshop/client'
 WEB_USER='www'
 WEB_USERGROUP='www'
 # we can do 
-echo "Start deployment git.wphkj.cn"
+echo "Start deployment vx.itnote.cn"
 cd $WEB_PATH
 echo "pulling source code..."
 # git reset --hard origin/release
 # git clean -f
-# 把项目拉取到最新
-git pull origin master
-# git checkout master
+git pull
+git checkout master
 echo "changing permissions..."
-# 切换到client里面
 cd $WEB_PATH_CLIENT
-# 把vue项目编译
 npm run build
 chown -R $WEB_USER:$WEB_USERGROUP $WEB_PATH_CLIENT
 echo "Finished."
